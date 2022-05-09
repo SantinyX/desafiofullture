9c5d8d2c0eba5638b4310e994c96b295246e7ca3
Santiny <diegosantiny@hotmail.com>
2022-05-09 18:58:08 -0300

    evento click

--- JS/Script.js
+++ JS/Script.js
@@ -1,4 +1,16 @@
-function adicionarTarefa(){
-    const adicionar = document.getElementById("addTarefa")
-    console.log(addTarefa.value)
+let adicionar = document.getElementById("addTarefa");
+let btn_adicionar = document.getElementById("add_tarefa");
+
+
+const exibirInformacao = function(){
+
+    if ( adicionar.value == ""){
+        alert("Tarefa não infomada!");
+    }
+    else{
+        console.log(adicionar.value);
+    }
 }
+
+btn_adicionar.addEventListener("click", exibirInformacao );
+
