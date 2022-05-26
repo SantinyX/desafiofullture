1986cf4076ce716b2f10f487eee3abf637f6fae6
Santiny <diegosantiny@hotmail.com>
2022-05-26 19:05:23 -0300

    projeto final

--- JS/Script.js
+++ JS/Script.js
@@ -1,40 +1,148 @@
-
 //Ajustar formulario e declarar as variaveis 
 const formulario = document.querySelector("form")
+let lista = Array()
+//Criando local storage para armazenamento de string.
+let atividadeArmazenada = localStorage.getItem("armazenamentoAtividade.atividade")
 
-formulario.addEventListener("submit", function(event){
-    //criar array para armazenar os itens
-    let adicionar = document.getElementById("addTarefa")
+let listaTemp = Array()
+if(atividadeArmazenada != null){
+    listaTemp = JSON.parse(atividadeArmazenada)
 
-    event.preventDefault();
-    //capturar os elementos digitado pelos usuários 
-       if ( adicionar.value == ""){
-        alert("Digite a tarefa");
-        return false;
-        }
-      
-    //criação do item
-    let item = {
-        addTarefa: adicionar.value, status:"Pendente"        
-    }
-
-    
-
-     console.log(item)
-
-})
-
-function addTarefa(event){
 
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("load");
+
+    lista.push({
+        tittle: "Estudar Algoritmo",
+        status: "uncompleted"
+    });
+    lista.push({
+        tittle: "Estuduar JavaScript",
+        status: "uncompleted"
+    });
+    lista.push({
+        tittle: "Entregar desafio JavaScript",
+        status: "uncompleted"
+    });
 
 
-//FUNCTION DOM(OBJ){}
-//criar tabela que já existe no HTML
-/*const form = documento.querySelector("local que deseja")*/
+    atualizar(lista);
+});
 
-//CRIAR LAÇOS ENTRE ELEMENTOS
+formulario.addEventListener("submit", function (event) {
 
-/**/
+    event.preventDefault();
+    let tarefa = document.getElementById('tarefa');
 
+    if (tarefa.value == "") {
+        alert("Preencha o campo")
+        return false
+    }else{
+        localStorage.setItem("armazenamentoAtividade.atividade"), JSON.stringify(lista)
+        lista.push
+    }
+
+    lista.push({
+        tittle: tarefa.value,
+        status: "uncompleted"
+    });
+    atualizar(lista);
+    console.log('lista', lista);
+});
+//seleção do status
+let selectStatus = document.getElementById("selectStatus");
+//evento change
+selectStatus.addEventListener('change', function (event) {
+    event.preventDefault();
+    
+    let itensTemp = new Array();
+    if (selectStatus.value === 'all') {
+        for (i = 0; i < lista.length; i++) {
+            itensTemp.push(lista[i]);
+        }    
+        atualizar(itensTemp);
+        return;
+    }
+
+    for (i = 0; i < lista.length; i++) {
+        if (lista[i].status === selectStatus.value) {
+            itensTemp.push(lista[i]);
+        }
+    }
+    atualizar(itensTemp);
+});
+
+//Função de criação da div do item
+function getItem(tittle, index) {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'todo');
+
+    //Criação do li para lista
+    let li = document.createElement('li');
+    div.appendChild(li);
+    li.appendChild(document.createTextNode(tittle));
+    li.setAttribute('class', 'todo-item')
+    //nome e valor
+    //cria
+    let btnCheck = document.createElement('button');
+    btnCheck.setAttribute('class', 'check-btn');
+    btnCheck.addEventListener('click', function (event) {
+        //função!!!!(index)
+        finalizar(index)
+    });
+    let iconCheck = document.createElement('i');
+    //pesquisar "aria-hidden"
+    iconCheck.setAttribute('class', 'fas fa-check');
+    iconCheck.setAttribute('aria-hidden', 'true');
+    //appendchild = item dentro de item
+    btnCheck.appendChild(iconCheck);
+    div.appendChild(btnCheck);
+
+    //criar botão trash
+    let btnTrash = document.createElement('button');
+    btnTrash.setAttribute('class', 'trash-btn');
+    btnTrash.addEventListener('click', function (event) {
+        //função!!!!(index)
+        remover(index)
+    });
+
+    let iconTrash = document.createElement('i');
+    iconTrash.setAttribute('class', 'fas fa-trash');
+    iconTrash.setAttribute('aria-hidden', 'true');
+    btnTrash.appendChild(iconTrash);
+    div.appendChild(btnTrash)
+    
+    return div;
+}
+
+function atualizar(itens) {
+    //Limpa a lista
+    let itensTemp = Array();
+    //atualizar lista
+    let todolist = document.querySelector(".todo-list");
+    todolist.textContent = "";
+    for (i = 0; i < itens.length; i++) {
+        itensTemp.push(itens[i]);    
+        todolist.appendChild(getItem(itens[i].tittle, i));
+    }
+    listaTemp = itensTemp;
+}
+
+function remover(index) {
+
+    console.log('index', index)
+    //remoção do item do array
+    listaTemp.shift(index)
+    console.log('listaTemp', listaTemp)
+    atualizar(listaTemp)
+}
+
+function finalizar(index) {
+    console.log('index', index)
+    //remoção do item do array
+    listaTemp[index].status = 'completed'
+    console.log('listaTemp', listaTemp)
+    atualizar(listaTemp)
+}
--- JS/Script2.js
+++ JS/Script2.js
@@ -3,22 +3,73 @@
 /*
 
 JSON O QUE É*
-
 Eviar para o DOM e armazenar para o localStorage
-
-
-
-
-
-
-
 IF(TAREFA != NULL){
     mais o que foi no array = localstorage
 }
+*/
+
+//CRIAÇÃO DO FORMULARIO E SEUS EVENTOS
+const formulario = document.querySelector('form')
+
+const listaAtividades = Array()
+
+//Adicionar o evento de formulário
+formulario.addEventListener("submit", function (event) {
+
+    const adicionar = document.getElementById('addTarefa')
+    
+    event.preventDefault()
+
+    if (adicionar.value == "");{
+        alert("Preencha o campo!")
+        return false
+    }
+
+    let item = {
+        addTarefa: adicionar.value,
+        status: "Pendente"
+    }
+    console.log(item)
+
+})
 
 
 
+function addElementToArray(taskArray) {
+    let div = document.createElement("div");
+    div.className = "todo"
+    
+    let li = document.createElement("li", "todo-list")
+    let inputValue = document.getElementById("task-input").value
+    let textNode = document.createTextNode(inputValue)
+        li.appendChild(textNode)
+        
+        li.className = "todo-item"
 
+  
+    for (let i = taskArray.length; i < taskArray.length+1; i++){
+        div.id = 'todo-list-div' + [i]
+        li.id = "todo-list-item" + [i]
+    }
+    //button setups
+    let buttonCheck = document.createElement("button")
+        buttonCheck.className="check-btn"
+        buttonCheck.setAttribute("id", "checked-btn");
+//sets the images of check button
+    let imageButton = document.createElement("i")
+        imageButton.className = "fas fa-check"
+        buttonCheck.appendChild(imageButton)
+//initializes trash button
+    let buttonTrash = document.createElement("button")
+        buttonTrash.className = "trash-btn"
+        buttonTrash.setAttribute("id", "delete-btn")
+       
+//sets the images of trash button
+    let imageTrashButton = document.createElement("i")
+        imageTrashButton.className = "fas fa-trash"
+        buttonTrash.appendChild(imageTrashButton)
+}
+//criar validação de recebimento
 
-
-*/    
+//Criar recebimento do input*/
